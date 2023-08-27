ef5edca9c55e354da11c2ccc699886941de4a3c2
MinhThang <Thanmn9403@gmail.com>
2023-08-27 17:23:32 +0700

    fix root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Providers from "@/context/Providers";
-import Footer from "./Footer";
+// import Footer from "./Footer";
 import "./globals.css";
 import Header from "./Header";
 
@@ -15,15 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="">
+      <body className=" scrollbar ">
         <Providers>
-          <div className="bg-[#f8f3ee] border-b border-gray-400 shadow-b shadow-md">
-            <Header />
-          </div>
-          <div className="fit">{children}</div>
-          <div className="bg-[#f8f3ee] border-t border-gray-400 shadow-t shadow-md">
-            <Footer />
-          </div>
+          <Header />
+          {children}
         </Providers>
       </body>
     </html>
