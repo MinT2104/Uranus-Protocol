f344c28b536f26b1340826ffca7d722a698bbeb2
MinhThang <Thanmn9403@gmail.com>
2023-08-27 17:25:38 +0700

    fix home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
+"use client";
+import { DefaultLayout } from "@/layouts/DefaultLayout";
 import Content from "./Content";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import { RootState } from "@/context/store";
+import { useEffect } from "react";
+import axiosClient from "@/services/axiosInstance";
 
 export default function Home() {
+  const wallet = useSelector((state: RootState) => state.wallet);
+  console.log(wallet.wallet?.accountId);
+  useEffect(() => {
+    axiosClient.get("/pump").then((res) => console.log(res));
+  }, []);
   return (
-    <main>
-      <Content/>
-    </main>
+    <section className="w-full h-fit bg-primary">
+      <div className="h-screen w-[500px] mx-auto flex flex-col items-center justify-center gap-4">
+        <h1 className="font-black text-[40px] text-pink-600 ">
+          Unarus Protocol
+        </h1>
+        <h3 className="font-light text-sm text-noSelected">
+          The platform offers reliable contracts
+        </h3>
+        <button className="font-bold text-lg text-white px-6 py-3 bg-pink-400 rounded-full">
+          <Link href="/jobs">Get started</Link>
+        </button>
+      </div>
+    </section>
   );
 }
